94bfc8559a574fe030a47d8c659c1b93cca2a629
NicoVill <o_sl_f@hotmail.com>
2015-11-16 10:25:32 +0100

    Carete Constructor for Tasks (N)

--- WebScrum/Tasks.cs
+++ WebScrum/Tasks.cs
@@ -10,7 +10,16 @@ namespace WebScrum
         public string  TaskName { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        public string TaksOwner { get; set; }
+        public string TasksOwner { get; set; }
         public int TaskPoints { get; set; }
+
+        public Tasks (string tn, DateTime st, DateTime et, string to, int tp)
+        {
+            TaskName = tn;
+            StartTime = st;
+            EndTime = et;
+            TasksOwner = to;
+            TaskPoints = tp;
+        }
     }
 }
